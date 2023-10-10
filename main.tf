feed3d7886ee272d69e40804b762ad19c8d4bca5
kislyakovIlya <super_fox@mail.ru>
2023-10-10 15:04:21 -0700

    update 4 hosts no generate inv

--- main.tf
+++ main.tf
@@ -79,10 +79,10 @@ output "caller_arn" {
 output "caller_user" {
   value = data.aws_caller_identity.current.user_id
 }
-resource "null_resource" "generate_inventory" {
-  depends_on = [aws_instance.my_Amazon_Linux]
-
-  provisioner "local-exec" {
-    command = "./generate_inventory.sh"
-  }
-}
+#resource "null_resource" "generate_inventory" {
+#  depends_on = [aws_instance.my_Amazon_Linux]
+#
+#  provisioner "local-exec" {
+#    command = "./generate_inventory.sh"
+#  }
+#}
