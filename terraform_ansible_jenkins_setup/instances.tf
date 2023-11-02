8438968fbdd00cf839469af28be1f19c0e9c5f3e
kislyakovIlya <super_fox@mail.ru>
2023-11-02 16:23:55 -0700

    0

--- terraform_ansible_jenkins_setup/instances.tf
+++ terraform_ansible_jenkins_setup/instances.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "jenkins" {
   instance_type          = "t3.mini" # Do not forget to turn off the instance after the test (56$ per month)
   vpc_security_group_ids = [aws_security_group.web-sg.id]
   key_name               = "sep_group"
-  iam_instance_profile = aws_iam_instance_profile.Jenkins_One.name
+  iam_instance_profile = aws_iam_instance_profile.jenkins_profile.name
 
   tags = {
     Name = "Jenkins_test"
