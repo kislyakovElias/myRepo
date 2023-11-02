07b22eb81c0aa35720bb13563295f2b1b0414a7c
kislyakovIlya <super_fox@mail.ru>
2023-11-01 22:53:10 -0700

    3

---
+++ terraform_gen_inst_setup_with_lb/instances.tf
@@ -0,0 +1,20 @@
+
+resource "aws_instance" "test_c6a_large_1" {
+  ami                    = "ami-053b0d53c279acc90" #Ubuntu server 22.04
+  instance_type          = "t3.small" // do not forget to turn off the instance after the test ( 56$ per month)
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  key_name               = "sep_group"
+  tags = {
+    Name = "Test_1"
+  }
+}
+
+resource "aws_instance" "test_c6a_large_2" {
+  ami                    = "ami-053b0d53c279acc90" #Ubuntu server 22.04
+  instance_type          = "t3.small" // do not forget to turn off the instance after the test ( 56$ per month)
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  key_name               = "sep_group"
+  tags = {
+    Name = "Test_2"
+  }
+}
