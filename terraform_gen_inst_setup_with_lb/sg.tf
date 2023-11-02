4c80acced8f20a46a35f6ec2ea6774d1b2547fbc
kislyakovIlya <super_fox@mail.ru>
2023-11-02 16:13:03 -0700

    7

--- terraform_gen_inst_setup_with_lb/sg.tf
+++ terraform_gen_inst_setup_with_lb/sg.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
   ingress {
     from_port   = 0
-    to_port     = 22
+    to_port     = 65000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
