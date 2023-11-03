a321f476cb00180cf112fd8497c89eaa5c854c9a
kislyakovIlya <super_fox@mail.ru>
2023-11-02 17:27:02 -0700

    11

--- terraform_ansible_jenkins_setup/instances.tf
+++ terraform_ansible_jenkins_setup/instances.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "jenkins" {
   ami                    = "ami-053b0d53c279acc90" # Ubuntu server 22.04
-  instance_type          = "t3.mini" # Do not forget to turn off the instance after the test (56$ per month)
+  instance_type          = "t3.small" # Do not forget to turn off the instance after the test (56$ per month)
   vpc_security_group_ids = [aws_security_group.web-sg.id]
   key_name               = "sep_group"
   iam_instance_profile = aws_iam_instance_profile.jenkins_profile.name
